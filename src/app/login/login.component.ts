6b9e9f1575b4562159bb0e8041fae5623d974c19
b <kpolanowski44@gmail.com>
2020-01-31 14:54:12 +0100

    refactor(login): added action after login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pfs-login',
@@ -9,18 +10,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class LoginComponent implements OnInit {
   public loginFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.loginFormGroup = this.formBuilder.group({
-      loginControl: [''],
+      loginControl: ['', Validators.required],
       passwordControl: [''],
     });
   }
 
   public submitLoginForm(): void {
     if (this.loginFormGroup.dirty && !!this.loginFormGroup.get('passwordControl').value) {
-      sessionStorage.setItem('login', this.loginFormGroup.get('loginControl').value);
+      sessionStorage.setItem('sessionLogin', this.loginFormGroup.get('loginControl').value);
+      this.router.navigateByUrl('/features');
     }
   }
 
